7b24c9e8d7ce3007619b230c26d476cff6bbbac9
sirojudin <sirojudin145@gmail.com>
2019-10-18 17:07:05 +0700

    initial repo

---
+++ app/Http/Controllers/MessageController.php
@@ -0,0 +1,20 @@
+<?php
+namespace App\Http\Controllers;
+use App\Events\MessageCreatedEvent;
+use Illuminate\Http\Request;
+class MessageController extends Controller
+{
+
+/**
+* Store a new message.
+*
+* @param  Request  $request
+* @return Response
+*/
+	public function store(Request $request)
+	{
+		$message = $request->message;
+		event(new MessageCreatedEvent($message));
+		return $message;
+	}
+}
